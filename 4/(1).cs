36ea2dace48672633b0e30fdfd23c891130fd35c
illidan2333 <66096895+illidan2333@users.noreply.github.com>
2020-06-04 20:09:18 +0800

    Add files via upload

---
+++ 4/(1).cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._28test
+{
+    class Node<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList() { head = tail = null; }
+        public Node<T> Head { get => head; }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> x = head; x != null; x = x.Next)
+            {
+                action(x.Data);
+            }
+        }
+    }
+}
---
+++ 4/(2).cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._28test
+{
+    public delegate void AlarmHandler(int timeTick);
+    public delegate void TickHandler(int timeTick);
+    class ClockEvent
+    {
+        public event AlarmHandler onAlarm;
+        public event TickHandler onTick;
+        public void Tick(int timeTick)
+        {
+            onTick(timeTick);
+        }
+        public void Alarm(int timeTick)
+        {
+            onAlarm(timeTick);
+        }
+    }
+
+    class AlarmClock
+    {
+        public ClockEvent clock = new ClockEvent();
+        private int timeTick;
+        private int TimeTick
+        {
+            get
+            {
+                return timeTick;
+            }
+            set
+
+            {
+                timeTick = value;
+            }
+        }
+        public AlarmClock()
+        {
+            timeTick = 0;
+            clock.onTick += new TickHandler(TickEvent);
+            clock.onAlarm += new AlarmHandler(AlarmEvent);
+        }
+        public void TickEvent(int timeTick)
+        {
+            Console.WriteLine(timeTick);
+        }
+        public void AlarmEvent(int timeTick)
+        {
+            Console.WriteLine(timeTick);
+        }
+        public void Tick()
+        {
+            clock.Tick(timeTick);
+        }
+        public void Alarm()
+        {
+            clock.Alarm(timeTick);
+        }
+    }
+}
