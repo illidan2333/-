fdb0e29b6981e2c2962c1604bd2baba6186e9185
illidan2333 <66096895+illidan2333@users.noreply.github.com>
2020-06-04 20:27:30 +0800

    Add files via upload

---
+++ 9/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Crawler___Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResultGridView = new System.Windows.Forms.DataGridView();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.ResultGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ResultGridView
+            // 
+            this.ResultGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ResultGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.index,
+            this.URL});
+            this.ResultGridView.Location = new System.Drawing.Point(13, 47);
+            this.ResultGridView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ResultGridView.Name = "ResultGridView";
+            this.ResultGridView.RowTemplate.Height = 23;
+            this.ResultGridView.Size = new System.Drawing.Size(634, 412);
+            this.ResultGridView.TabIndex = 5;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(111, 18);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(454, 25);
+            this.txtUrl.TabIndex = 7;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(573, 14);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(74, 29);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "扫描";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "请输入网址";
+            // 
+            // index
+            // 
+            this.index.DataPropertyName = "Index";
+            this.index.HeaderText = "No.";
+            this.index.Name = "index";
+            // 
+            // URL
+            // 
+            this.URL.DataPropertyName = "URL";
+            this.URL.HeaderText = "URL";
+            this.URL.Name = "URL";
+            this.URL.Width = 500;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 472);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResultGridView);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ResultGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ResultGridView;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+    }
+}
+
---
+++ 9/SimpleCrawler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleCrawler
+{
+    class Crawler
+    {
+        public event Action<Crawler, string> PageDownloaded;
+
+        public Hashtable urls = new Hashtable();
+        private int count = 0;
+        static public string startUrl = "";
+        static public string startWith = "";
+
+        public void Crawl()
+        {
+            string str = @"(www\.){0,1}.*?\..*?/";
+            Regex r = new Regex(str);
+            Match m = r.Match(startUrl);
+            startWith = m.Value;
+
+
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+
+                if (current == null || count > 20) break;
+
+                string html = DownLoad(current); 
+                urls[current] = true;
+                count++;
+                PageDownloaded(this, current);
+                Parse(html, current);
+
+            }
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+         
+                return "";
+            }
+        }
+
+        private void Parse(string html, string oldUrl)
+        {
+
+            
+            string strRef = @"(href|HREF)[ ]*=[ ]*[""'](http|https)[^""'#>]+..html.*?[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (url.Length == 0)
+                    continue;
+            
+                if (url.Contains(startWith))
+                {
+                    if (urls[url] == null)
+                        urls[url] = false;
+                }
+            }
+
+  
+
+            strRef = @"(href|HREF)[ ]*=[ ]*[""'][^(http|https)][^""'#>]+..html.*?[""']";
+            matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (url.Length == 0) continue;
+                Uri baseUri = new Uri(oldUrl);
+                Uri absoluteUri = new Uri(baseUri,url);
+                Console.WriteLine("相对:" + url);
+                Console.WriteLine("绝对:" + absoluteUri.ToString());
+                if (urls[absoluteUri.ToString()] == null)
+                    urls[absoluteUri.ToString()] = false;
+            }
+        }
+    }
+}
